c85c43e8e2bae581b5d500ff484eabf1897f0335
ShresthaRujal <shrestharujal1928@gmail.com>
2023-09-26 21:35:43 +0545

    updated github workflow

--- src/heading.rs
+++ src/heading.rs
@@ -4,9 +4,9 @@ use dioxus::prelude::*;
 
 #[inline_props]
 pub fn Heading(cx: Scope) -> Element {
-	render!(
-		div {
-			h3 { class: "w-full h-screen bg-gray-300 flex items-center justify-center", "Hello, World!" }
-		}
-	)
+    render!(
+        div {
+            h3 { class: "w-full h-screen bg-gray-300 flex items-center justify-center", "Hello, World!" }
+        }
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use dioxus::prelude::*;
 use heading::Heading;
 
 fn main() {
-	// Dioxus renders app in Webview
-	dioxus_desktop::launch(App);
+    // Dioxus renders app in Webview
+    dioxus_desktop::launch(App);
 }
 
 // Define a component that renders a div
 fn App(cx: Scope) -> Element {
-	// The render! macro makes it easy for developers to write jsx-style markup in their components.
-	render! {
-		link { rel: "stylesheet", href: "../dist/output.css" },
-		Heading {}
-	}
+    // The render! macro makes it easy for developers to write jsx-style markup in their components.
+    render! {
+        link { rel: "stylesheet", href: "../dist/output.css" },
+        Heading {}
+    }
 }
